ea916a5e2160093fd54f661f4490c5473a3c50d3
MarcosCerioni <marcoscerioni98@gmail.com>
2019-06-04 19:19:59 -0300

    Arreglando front y ya sunrise and sunset devuelven el tiempo correcto

--- src/components/Tabs/TabsContent/Current.jsx
+++ src/components/Tabs/TabsContent/Current.jsx
@@ -31,19 +31,26 @@ class Current extends React.Component{
             pressure : result.main.pressure,
             humidity : result.main.humidity,
             wind: result.wind.speed,
+            temperature : result.main.temp,
             temperature_min: result.main.temp_min,
             temperature_max : result.main.temp_max,
             sunrise: result.sys.sunrise,
             sunset : result.sys.sunset,
             loading: false, error: false
+
           });
           console.log(result)
         }).catch((error) => {
-          this.setState({ error: true });
-        });
+      this.setState({ error: true });
+    });
   }
 
   render(){
+    const convert = (props) => {
+      let date = new Date(1000*props);
+      return (date.toTimeString().split(' ')[0])
+    };
+
     if (this.state.error) {
       return <ErrorMessage visible={true} message={"An error occurred"}/>
     }
@@ -54,28 +61,36 @@ class Current extends React.Component{
     }
 
     return(
-      <div className="weather-info" >
-        <p className="weather__key">Pressure:
-          <span className="weather__value"> {this.state.pressure + "hpm"}</span>
-        </p>
-        <p className="weather__key">Humidity:
-          <span className="weather__value">  {this.state.humidity + "%"}</span>
-        </p>
-        <p className="weather__key">Wind:
-          <span className="weather__value">  {this.state.wind + "km/h"}</span>
-        </p>
-        <p className="weather__key">TempMin:
-          <span className="weather__value">  {this.state.temperature_min + "°C"}</span>
-        </p>
-        <p className="weather__key">TempMax:
-          <span className="weather__value">  {this.state.temperature_max + "°C"}</span>
-        </p>
-        <p className="weather__key">Sunrise:
-          <span className="weather__value">  {this.state.sunrise}</span>
-        </p>
-        <p className="weather__key">Sunset:
-          <span className="weather__value">  {this.state.sunset}</span>
-        </p>
+      <div>
+          <button className={"current"}> Current </button>
+          <button className={"forecast"}> Forecast </button>
+          <button className={"uvi"}> UVI </button>
+        <div className="weather-info" >
+          <p className="weather__key">Pressure:
+            <span className="weather__value"> {this.state.pressure + "hpm"}</span>
+          </p>
+          <p className="weather__key">Humidity:
+            <span className="weather__value">  {this.state.humidity + "%"}</span>
+          </p>
+          <p className="weather__key">Wind:
+            <span className="weather__value">  {this.state.wind + "km/h"}</span>
+          </p>
+          <p className="weather__key">Temperature:
+            <span className="weather__value">  {this.state.temperature + "°C"}</span>
+          </p>
+          <p className="weather__key">TempMin:
+            <span className="weather__value">  {this.state.temperature_min + "°C"}</span>
+          </p>
+          <p className="weather__key">TempMax:
+            <span className="weather__value">  {this.state.temperature_max + "°C"}</span>
+          </p>
+          <p className="weather__key">Sunrise:
+            <span className="weather__value">  {convert(this.state.sunrise)}</span>
+          </p>
+          <p className="weather__key">Sunset:
+            <span className="weather__value">  {convert(this.state.sunset)}</span>
+          </p>
+        </div>
       </div>
     )
   }
