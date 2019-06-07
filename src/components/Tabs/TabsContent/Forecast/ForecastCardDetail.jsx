d1888d7e3f11ee1dec707851ca2061ef137b37f1
alexislevi96 <alexislevi96@hotmail.com>
2019-06-06 22:52:58 -0300

    Empezando a parsear los dias

--- src/components/Tabs/TabsContent/Forecast/ForecastCardDetail.jsx
+++ src/components/Tabs/TabsContent/Forecast/ForecastCardDetail.jsx
@@ -8,13 +8,16 @@ export default class ForecastCardDetail extends Component {
 
   render() {
     console.log("FORECAST DETAIL");
-    console.log(this.props.details);
+    console.log(this.props.details.filter(function(item){
+      return (item.dt_txt.split(" ")[1] === "00:00:00")})[0].dt_txt.split(" ")[0]);
     return (
       <div onClick={this.onBackClicked.bind(this)}>
-        {this.props.details.map((detail) =>
+        {this.props.details.filter(function(item){
+          return (item.dt_txt.split(" ")[1] === "00:00:00")}).map((detail) =>
           <div>
-            Day:
-            {detail.dt_txt}
+            Day ver clima cada tres horas:
+            {detail}
+
           </div>
         )}
       </div>
